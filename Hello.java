f143c8ec3242067f812d60585d14926c2cb9ed8b
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2020-10-20 16:37:50 -0400

    initial files for youtube jenkins tutorial to trigger build via SCM polling

---
+++ Hello.java
@@ -0,0 +1,11 @@
+// Integrating Git with Jenkins: https://www.youtube.com/watch?v=bGqS0f4Utn4&list=PLhW3qG5bs-L_ZCOA4zNPSoGbnVQ-rp_dG&index=8
+
+public class Hello {
+
+  public static void main(String[] args){
+    for(int i = 1; i <= 7; i++) {
+      System.out.println("Hellow World ... + " + i);
+    }
+  }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# About
+[Raghav Pal's](https://www.youtube.com/playlist?list=PLhW3qG5bs-L_ZCOA4zNPSoGbnVQ-rp_dG) Jenkins Tutorial Youtube Series. 
