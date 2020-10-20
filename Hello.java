143eebcc3dbe0d5607ef16d3e430f15013efb2c3
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2020-10-20 16:41:13 -0400

    slight Hello file modification to trigger Jenkins job build via SCM polling

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@
 public class Hello {
 
   public static void main(String[] args){
-    for(int i = 1; i <= 7; i++) {
+    for(int i = 1; i <= 5; i++) {
       System.out.println("Hellow World ... + " + i);
     }
   }
